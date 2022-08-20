c18518bd392679c11d99d3b0e4c7a0c7a9c6e8a0
AttilaLeNain <lerouvillois.kevin@gmail.com>
2022-08-21 00:14:15 +0200

    Update ColoringGame-AutoColor.py

--- ColoringGame-AutoColor.py
+++ ColoringGame-AutoColor.py
@@ -1,4 +1,3 @@
-from tkinter import Button
 import pyautogui
 import os
 from pathlib import Path
@@ -9,10 +8,12 @@ import win32api, win32con
 main_path = Path(__file__).parent
 
 while 1:
-    if keyboard.is_pressed("P"):
-	    break
 
-    else:
-        coordonnées = pyautogui.locateCenterOnScreen(f'{main_path}\image\-Color.png', grayscale=False, confidence=0.9)
-        pyautogui.leftClick(coordonnées)
-        pyautogui.rightClick(coordonnées)
+    img = pyautogui.locateCenterOnScreen(f'{main_path}\image\-Color.png', grayscale=False, confidence=0.9)
+
+    if keyboard.is_pressed("P"):
+        break
+
+    if img is not None:
+        pyautogui.leftClick(img)
+        pyautogui.rightClick(img)
