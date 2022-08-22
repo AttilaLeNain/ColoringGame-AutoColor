0a2c3e91e33ab99470b82d4d188edc5f9b99d9d1
AttilaLeNain <lerouvillois.kevin@gmail.com>
2022-08-22 15:23:49 +0200

    Update

--- ColoringGame-AutoColor.py
+++ ColoringGame-AutoColor.py
@@ -1,14 +1,17 @@
 import pyautogui
+from pynput.mouse import Controller
 import os
 from pathlib import Path
 import time
 import keyboard
 import win32api, win32con
 
+mouse= Controller()
 main_path = Path(__file__).parent
 
 while 1:
-
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
     img = pyautogui.locateCenterOnScreen(f'{main_path}\image\-Color.png', grayscale=False, confidence=0.9)
 
     if keyboard.is_pressed("P"):
@@ -17,3 +20,4 @@ while 1:
     if img is not None:
         pyautogui.leftClick(img)
         pyautogui.rightClick(img)
+        print(current_time,"Une case en",mouse.position)
